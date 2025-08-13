5e9f3b45a0b53669679404cb94998c710d14af61
Bogdan Boksan <bogdan.boksan@ananas.rs>
2025-08-13 12:41:54 +0200

    fix: conditionally include Swift bridge functionality based on availability

--- ios/MsuCseV2.mm
+++ ios/MsuCseV2.mm
@@ -1,11 +1,17 @@
 #import "MsuCseV2.h"
 
-#ifdef RCT_NEW_ARCH_ENABLED
+// Import Swift header only when available
+#if __has_include("MsuCseV2-Swift.h")
 #import "MsuCseV2-Swift.h"
+#define SWIFT_BRIDGE_AVAILABLE 1
+#else
+#define SWIFT_BRIDGE_AVAILABLE 0
 #endif
 
 @implementation MsuCseV2 {
+#if SWIFT_BRIDGE_AVAILABLE
     CSEBridge *_cseBridge;
+#endif
 }
 
 RCT_EXPORT_MODULE()
@@ -13,7 +19,9 @@ RCT_EXPORT_MODULE()
 - (instancetype)init {
     self = [super init];
     if (self) {
+#if SWIFT_BRIDGE_AVAILABLE
         _cseBridge = [[CSEBridge alloc] init];
+#endif
     }
     return self;
 }
@@ -21,39 +29,69 @@ RCT_EXPORT_MODULE()
 // MARK: - Initialization
 
 - (void)initialize:(BOOL)developmentMode {
+#if SWIFT_BRIDGE_AVAILABLE
     [_cseBridge initializeWithDevelopmentMode:developmentMode];
+#endif
 }
 
 // MARK: - Validation Methods
 
 - (BOOL)isValidCVV:(NSString *)cvv {
+#if SWIFT_BRIDGE_AVAILABLE
     return [_cseBridge isValidCVV:cvv];
+#else
+    return NO;
+#endif
 }
 
 - (BOOL)isValidCVVWithPan:(NSString *)cvv pan:(NSString *)pan {
+#if SWIFT_BRIDGE_AVAILABLE
     return [_cseBridge isValidCVVWithPanWithCvv:cvv pan:pan];
+#else
+    return NO;
+#endif
 }
 
 - (BOOL)isValidCardHolderName:(NSString *)name {
+#if SWIFT_BRIDGE_AVAILABLE
     return [_cseBridge isValidCardHolderName:name];
+#else
+    return NO;
+#endif
 }
 
 - (BOOL)isValidPan:(NSString *)pan {
+#if SWIFT_BRIDGE_AVAILABLE
     return [_cseBridge isValidPan:pan];
+#else
+    return NO;
+#endif
 }
 
 - (BOOL)isValidCardToken:(NSString *)token {
+#if SWIFT_BRIDGE_AVAILABLE
     return [_cseBridge isValidCardToken:token];
+#else
+    return NO;
+#endif
 }
 
 - (BOOL)isValidExpiry:(double)month year:(double)year {
+#if SWIFT_BRIDGE_AVAILABLE
     return [_cseBridge isValidExpiryWithMonth:(int)month year:(int)year];
+#else
+    return NO;
+#endif
 }
 
 // MARK: - Card Brand Detection
 
 - (NSString *)detectBrand:(NSString *)pan {
+#if SWIFT_BRIDGE_AVAILABLE
     return [_cseBridge detectBrand:pan];
+#else
+    return @"";
+#endif
 }
 
 // MARK: - Encryption Methods (Async)
@@ -63,6 +101,7 @@ RCT_EXPORT_MODULE()
            resolve:(RCTPromiseResolveBlock)resolve
             reject:(RCTPromiseRejectBlock)reject {
     
+#if SWIFT_BRIDGE_AVAILABLE
     [_cseBridge encryptCvvWithCvv:cvv nonce:nonce callback:^(NSString * _Nullable encrypted, NSString * _Nullable error) {
         if (error) {
             reject(@"ENCRYPTION_ERROR", error, nil);
@@ -70,6 +109,9 @@ RCT_EXPORT_MODULE()
             resolve(encrypted);
         }
     }];
+#else
+    reject(@"SWIFT_BRIDGE_UNAVAILABLE", @"Swift bridge not available", nil);
+#endif
 }
 
 - (void)encryptCard:(NSString *)pan
@@ -81,6 +123,7 @@ RCT_EXPORT_MODULE()
            resolve:(RCTPromiseResolveBlock)resolve
             reject:(RCTPromiseRejectBlock)reject {
     
+#if SWIFT_BRIDGE_AVAILABLE
     [_cseBridge encryptCardWithPan:pan
                    cardHolderName:cardHolderName
                        expiryYear:(int)expiryYear
@@ -94,16 +137,27 @@ RCT_EXPORT_MODULE()
             resolve(encrypted);
         }
     }];
+#else
+    reject(@"SWIFT_BRIDGE_UNAVAILABLE", @"Swift bridge not available", nil);
+#endif
 }
 
 // MARK: - Error Handling
 
 - (NSArray<NSString *> *)getErrors {
+#if SWIFT_BRIDGE_AVAILABLE
     return [_cseBridge getErrors];
+#else
+    return @[];
+#endif
 }
 
 - (BOOL)hasErrors {
+#if SWIFT_BRIDGE_AVAILABLE
     return [_cseBridge hasErrors];
+#else
+    return NO;
+#endif
 }
 
 #ifdef RCT_NEW_ARCH_ENABLED
